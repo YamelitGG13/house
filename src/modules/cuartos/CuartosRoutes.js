07d1e3534b83906aa1dcb90330732ef11a6add08
Rita1313 <20230371@utsh.edu.mx>
2026-03-12 22:45:07 -0600

    Ruta de cuartos
    Ruta de cuartos

---
+++ src/modules/cuartos/CuartosRoutes.js
@@ -0,0 +1,8 @@
+import { Route } from 'react-router-dom';
+import Cuartos from './cuartos';
+
+const CuartosRoutes = (
+    <Route path="/cuartos" element={<Cuartos />} />
+);
+
+export default CuartosRoutes;
